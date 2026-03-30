2764f1fafd93e11f93f55c4c6d7fb967bdfdba74
alaskann <derproka@gmail.com>
2026-03-30 18:05:02 -0500

    add loader component

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -1,8 +1,21 @@
 export default function Loader() {
 	return (
-		<p>
-			<span {...{ "is-": "spinner" }} aria-hidden />
-			{" Loading…"}
-		</p>
+		<div
+			style={{
+				position: "fixed",
+				inset: 0,
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				gap: "0.5lh",
+			}}
+		>
+			<span
+				{...{ "is-": "spinner", "variant-": "dots", "speed-": "medium" }}
+				aria-hidden
+			/>
+			<span style={{ opacity: 0.6 }}>loading...</span>
+		</div>
 	);
 }
